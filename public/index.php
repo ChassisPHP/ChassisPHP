cc63f9a90ab0ca796903b631357b75d7cac6c67c
Roger Creasy <roger.creasy@gmail.com>
2017-08-08 21:57:49 -0400

    refactor routes to array file

--- public/index.php
+++ public/index.php
@@ -20,22 +20,6 @@ require __DIR__.'/../lib/Framework/startApp.php';
     // Use the Core to bootstrap an app
     $app = $container->get('Core');
 
-    //set up the routes (TODO refactor)
-    $app->addRoute('GET', '/', function () {
-            return new Response('This is the home page!');
-    });
-    $app->addRoute('GET', '/about', function () {
-        return new Response('This is the about page!');
-    });
-    $app->addRoute('GET', '/name/{name}', function ($name) {
-        return new Response('Well, Hello ' . $name);
-    });
-    $app->addRoute('GET', '/test', function () {
-        return new Response('Roger\'s test page?');
-    });
-    $app->addRoute('GET', '/errors/404', function () {
-        return new Response('Dude - there aint no setch page<br>That\'s a 404');
-    });
     $response = $app->run();
     
     echo $response;
