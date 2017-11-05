df7e02caf32c2d321d8a63e2d04def353914acee
Roger Creasy <roger.creasy@gmail.com>
2017-11-05 07:13:35 -0500

    add backend Nav; set links to existing pages

---
+++ resources/views/backend/partials/nav.twig.php
@@ -0,0 +1,12 @@
+<nav class="container blueBackground">
+    <ul class="nav item flexStart">
+        <li><a href="#">ChassisPHP</a></li>
+    </ul>
+    <ul class="nav container flexEnd">
+        <li><a href="#">Home</a></li>
+        <li><a href="/backend/users">Users</a></li>
+        <li><a href="/backend/register">Add User</a></li>
+        <li><a href="#">Careers</a></li>
+        <li><a href="#">Logout</a></li>
+    </ul>
+</nav>
--- resources/views/backend/partials/users.php
+++ resources/views/backend/partials/users.php
@@ -1,13 +1,13 @@
 {% extends 'backend/layout.twig.php' %}
 {% block content %} 
 <?php print_r($users); ?>
-<article class="container itemCenter">
+<article class="container itemCenter column">
 
-    <header class="item itemCenter">
+    <header class="item">
         <h1>Active User Accounts</h1>
     </header>
   
-    <div class="item itemCenter">
+    <div class="item">
         <ul>
         {% for user in users %}
             <li>{{ user.Id }} {{ user.UserName }} {{ user.Email }} {{ user.userLevel }}</li>
