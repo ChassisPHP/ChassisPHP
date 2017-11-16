65a785aabe318b1e9c731d1547dcc65a46f8bd22
Roger Creasy <roger.creasy@gmail.com>
2017-11-16 05:58:33 -0500

    prevent duplicate emails in db

--- lib/Database/Connection.php
+++ lib/Database/Connection.php
@@ -5,6 +5,9 @@ namespace Lib\Database;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use Lib\Framework\Config;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 class Connection
 {
@@ -15,7 +18,7 @@ class Connection
     public function __construct()
     {
         $this->config = new Config;
-        $connectionConfig = Setup::createAnnotationMetadataConfiguration(array(dirname(__FILE__, 3)."/Database/Entities"), $this->isDevMode);
+        //$connectionConfig = Setup::createAnnotationMetadataConfiguration(array(dirname(__FILE__, 3)."/Database/Entities"), $this->isDevMode);
         $driver = envar('DATABASE_DRIVER', 'pdo_mysql');
         $dbType = envar('DATABASE_TYPE', 'mysql');
         $path = dirname(__FILE__, 3) . "/" . $this->config[$dbType]['database'];
@@ -23,6 +26,13 @@ class Connection
                 'driver' => $driver,
                 'path' => $path,
             );
+
+        $connectionConfig = Setup::createConfiguration($this->isDevMode);
+        $connectionDriver = new AnnotationDriver(new AnnotationReader(), dirname(__FILE__, 3)."/Database/Entities");
+        
+        // registering noop annotation autoloader - allow all annotations by default
+        AnnotationRegistry::registerLoader('class_exists');
+        $connectionConfig->setMetadataDriverImpl($connectionDriver);
 
         // obtaining the entity manager
         $this->entityManager = EntityManager::create($conn, $connectionConfig);
