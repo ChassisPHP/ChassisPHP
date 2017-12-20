c2c3b1ab4a69faeea16bdaacacb69de76a012fc7
Roger Creasy <roger.creasy@gmail.com>
2017-12-20 07:09:35 -0500

    center and style welcome text on homepage

--- resources/views/frontend/pages/welcome.twig.php
+++ resources/views/frontend/pages/welcome.twig.php
@@ -1,9 +1,7 @@
 {% extends 'frontend/layout.twig.php' %}
 {% block content %}
 
-<article class="main-content">
-    <div class="container center">
-    <div class="banner"><h1>Welcome to ChassisPHP!</h1></div>
-    </div>
-</article>
+<div class="container center banner itemCenter">
+    <div class="textGrey">Welcome to ChassisPHP!</div>
+</div>
 {% endblock %}
