b0f205896d1742cf1afd6f6f2eec7db58f8f9626
Roger Creasy <roger.creasy@gmail.com>
2018-01-26 05:01:33 -0500

    add href value to nav brand and home menu item

--- resources/views/backend/partials/nav.twig.php
+++ resources/views/backend/partials/nav.twig.php
@@ -1,9 +1,9 @@
 <nav class="container blueBackground">
     <ul class="nav item flexStart">
-        <li><a href="#">ChassisPHP</a></li>
+        <li><a href="/">ChassisPHP</a></li>
     </ul>
     <ul class="nav container flexEnd">
-        <li><a href="#">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="/backend/users">Users</a></li>
         <li><a href="/backend/register">Add User</a></li>
         <li><a href="#">Careers</a></li>
