2e31dfb6c1ee1fa9aa000847c133dc8c96cdd1b4
Roger Creasy <roger.creasy@gmail.com>
2018-02-03 10:21:53 -0500

    fix merge conflicts

--- resources/views/backend/pages/contentDetails.twig.php
+++ resources/views/backend/pages/contentDetails.twig.php
@@ -15,10 +15,10 @@
     <a href="/backend/content" class="button blueBackground">All Content</a>
     <div class="col60">
         <div class="item itemCenter">
-            <h1>{{ entryDetails.Title }}</h1>
-            <h3>{{ entryDetails.Position }}</h3>
-            <p>{{ entryDetails.Body }}</p>
-            <div class="Rtable-cell">{{ entryDetails.PublicationDate|date('Y-m-d H:i:s') }}</div>
+            <h1 class="">{{ content.Title }}</h1>
+            <p>{{ content.Body }}</p>
+            <div class="Rtable-cell">{{ content.PublicationDate|date('Y-m-d H:i:s') }}</div>
+            <button class="button blueBackground"><a href="/backend/content/update/{{ content.ID }}">Edit</a></button>
             <button class="button redBackground"><a href="/backend/content/delete/{{ content.ID }}" onclick="return confirm('Are you sure?')">DELETE</a></button>
             </div>
         </div>
